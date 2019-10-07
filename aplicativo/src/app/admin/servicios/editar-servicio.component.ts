e00e3e4397d2b6527a1f1df798bc1284b008df86
Brandon Carmona <brandonvasco003@gmail.com>
2019-10-06 22:37:10 -0500

    Tablas y login

--- aplicativo/src/app/admin/servicios/editar-servicio.component.ts
+++ aplicativo/src/app/admin/servicios/editar-servicio.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {SelectionModel} from '@angular/cdk/collections';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+export interface elemento {
+  precio: number;
+  vehiculos: string;
+}
+
+const ELEMENT_DATA: elemento[] = [
+  {vehiculos: "vehiculos", precio: 1.2},
+  {vehiculos: "vehiculos", precio: 12},
+  {vehiculos: "vehiculos", precio: 12},
+  {vehiculos: "vehiculos", precio: 12},
+  {vehiculos: "vehiculos", precio: 1.2},
+  {vehiculos: "vehiculos", precio: 1.2},
+];
 
 @Component({
   selector: 'app-editar-servicio',
@@ -7,9 +25,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarServicioComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['vehiculos', 'precio', 'select'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  selection = new SelectionModel<elemento>(true, []);
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+    isAllSelected() {
+      const numSelected = this.selection.selected.length;
+      const numRows = this.dataSource.data.length;
+      return numSelected === numRows;
+    }
+  
+    masterToggle() {
+      this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+    }
+  
+   
+    checkboxLabel(row?: elemento): string {
+      if (!row) {
+        return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+      }
+      return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.vehiculos+ 1}`;
+    }
 }
