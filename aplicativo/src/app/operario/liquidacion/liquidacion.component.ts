4d9164d017256cc16c93c56096030d02154d18e3
Kevin Muentes <kevin.muentes@quipux.com>
2019-10-07 15:51:51 -0500

    cambios

--- aplicativo/src/app/operario/liquidacion/liquidacion.component.ts
+++ aplicativo/src/app/operario/liquidacion/liquidacion.component.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+export interface factura{
+  NR:String,
+  Nombre:String,
+  precio: number
+}
 
-
+const DataElement: factura[] =[
+  {
+    NR:"1",
+    Nombre:"lavado",
+    precio:10.000
+  },
+  {
+    NR:"2",
+    Nombre:"Cambio Aceite",
+    precio:10.000
+  },
+  {
+    NR:"3",
+    Nombre:"revision tecnica",
+    precio:10.000
+  },
+  {
+    NR:"4",
+    Nombre:"cambio llantas",
+    precio:10.000
+  },
+  {
+    NR:"5",
+    Nombre:"llantas",
+    precio:10.000
+  }
+];
 @Component({
   selector: 'app-liquidacion',
   templateUrl: './liquidacion.component.html',
   styleUrls: ['./liquidacion.component.css']
 })
+
+
 export class LiquidacionComponent implements OnInit {
     displayedColumns = [];
-    dataSource = [
-      {posicion:1,nombre:"lavado",precio:"$10.000"}
-    ]
+    dataSource = DataElement
   constructor() {
    this.displayedColumns = ['NR','Nombre','precio'];
    this.dataSource 
@@ -19,4 +50,7 @@ export class LiquidacionComponent implements OnInit {
   ngOnInit() {
   }
 
+  getTotalCost() {
+    return this.dataSource.map(t => t.precio).reduce((acc, value) => acc + value, 0);
+  }
 }
