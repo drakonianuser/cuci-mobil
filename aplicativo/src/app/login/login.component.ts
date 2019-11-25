6120705a292036bab8cb236b5b4e94fe46e9d315
brandon.carmona <brandon.carmona@quipux.com>
2019-11-25 08:21:31 -0500

    Se inicia la automatizacion y se añaden id

--- aplicativo/src/app/login/login.component.ts
+++ aplicativo/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {UsuariosService} from '../services/usuarios.service';
 import {usuario} from '../models/usuarios';
 import {Router} from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +11,24 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private userService: UsuariosService, private router: Router) { }
+  createFormGroup(){
+
+    return new FormGroup({
+      
+      user: new FormControl('',Validators.required),
+      password: new FormControl('',Validators.required),
+      boton: new FormControl('')
+
+    });
+
+  }
+
+  contactForm: FormGroup;
+  constructor(private userService: UsuariosService, private router: Router) {
+
+    this.contactForm = this.createFormGroup();
+
+   }
   usuario: usuario = {
     idUSUARIOS: null,
     USUARIO: '',
@@ -24,6 +43,11 @@ export class LoginComponent implements OnInit {
       localStorage.removeItem('usuario')
     }
   }
+  validacion(valid){
+
+    return valid
+
+  }
   login(){
     this.userService.getOneUser(this.usuario.USUARIO)
       .subscribe(
@@ -34,6 +58,7 @@ export class LoginComponent implements OnInit {
             this.router.navigateByUrl('/registroVehiculo')
             localStorage.setItem('usuario', JSON.stringify(this.usuarioObtenido))
             var usuarioS = JSON.parse(localStorage.getItem('usuario'))
+            this.validacion
             return "Se logea"
           }else{ 
             alert("Credenciales invalidas")
@@ -46,5 +71,22 @@ export class LoginComponent implements OnInit {
         }
       )
   }
+  onResetForm(){
+
+    this.contactForm.reset();
+
+  }
+  onSaveForm(){
+    if(this.contactForm.valid){
+      this.onResetForm();
+    }else{
+      console.log("Error")
+
+    }
+  }
+
+  get user(){return this.contactForm.get('user');}
+  get password(){return this.contactForm.get('password');}
+  get boton(){return this.contactForm.get('boton')}
 
 }
